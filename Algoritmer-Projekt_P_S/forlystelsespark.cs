3b826abd0b0c0aefd6e9508a52b89da80187b92d
Philip Bryde <brydephilip@gmail.com>
2026-02-06 12:12:50 +0100

    forlystelsespark

---
+++ Algoritmer-Projekt_P_S/forlystelsespark.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmer_Projekt_P_S
+{
+    internal class forlystelsespark
+    {
+
+    }
+}
